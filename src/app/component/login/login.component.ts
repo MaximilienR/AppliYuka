25d986e8ad435618d9edca8ca3802f51b1e758db
Maximilien Roussel <ro.maximilien@gmail.com>
2022-09-06 18:37:11 +0200

    add login function

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/service/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  erreur:boolean=false
 
-   constructor() { }
+   constructor(private route:Router,private loginService:LoginService) { }
     log(){}
 
 
   ngOnInit(): void {
+  }
+  password(val:any){
+    if(val.target.value=="sudo"){
+      this.erreur=false;
+      this.loginService.longin();
+      this.route.navigate(['/scan'])
+    }else{
+      this.erreur=true
+    }
   }
 
 }
