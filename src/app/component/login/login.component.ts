258c48941791596ba629546fa8c953535e2a7a47
Maximilien Roussel <ro.maximilien@gmail.com>
2022-09-07 10:05:21 +0200

    update longin control

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/service/login.service';
 
@@ -9,12 +10,20 @@ import { LoginService } from 'src/app/service/login.service';
 })
 export class LoginComponent implements OnInit {
   erreur:boolean=false
+  condition!:FormGroup
 
-   constructor(private route:Router,private loginService:LoginService) { }
+   constructor(private route:Router,private loginService:LoginService,private fb:FormBuilder) { }
     log(){}
 
 
   ngOnInit(): void {
+
+  this.condition=this.fb.group({
+    mail:this.fb.control('',[Validators.required])
+  })
+  }
+  get mail(){
+    return this.condition.get('mail')
   }
   password(val:any){
     if(val.target.value=="sudo"){
